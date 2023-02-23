3ab58bd509f4b352dbc74e56fbcb20cecde95752
averma2514 <79777996+averma2514@users.noreply.github.com>
2023-02-23 12:15:11 +0530

    Update index.php
    css file name change

--- home/index.php
+++ home/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant</title>
-    <link rel="stylesheet" href="main - Copy.css">
+    <link rel="stylesheet" href="index.css">
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@400;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1f6f99be7a.js" crossorigin="anonymous"></script>
 </head>
